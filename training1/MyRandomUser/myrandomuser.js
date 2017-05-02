22b112162896f54b8b32a0541ec52751d40c14d5
Mark Meijer <mjcmeije@avans.nl>
2017-05-02 07:54:29 +0200

    Initial commit

--- training1/MyRandomUser/myrandomuser.js
+++ training1/MyRandomUser/myrandomuser.js
@@ -5,10 +5,6 @@
 var express = require('express');
 var app = express();
 
-app.all('*', function(request, response) {
-    response.status(404);
-    response.send('404 - Not found');
-})
 
 app.get('/', function(request, response) {
     response.send('Hello Avans!');
@@ -26,7 +22,10 @@ app.get('/about', function(request, response) {
 //     response.send('Hello Avans, PUT request received!');
 // })
 
-
+app.all('*', function(request, response) {
+    response.status(404);
+    response.send('404 - Not found');
+})
 
 app.listen(3000, function() {
     console.log('Server app is listening on port 3000');
