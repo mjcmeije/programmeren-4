c217d1f50565e0326c88c319d583e37a93ce1334
Mark Meijer <mjcmeije@avans.nl>
2017-05-04 11:39:48 +0200

    Initial commit

--- training1/MyRandomUser/myrandomuser.js
+++ training1/MyRandomUser/myrandomuser.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var app = express();
 
+app.set('PORT', config.webPort);
 
 app.get('/', function(request, response) {
     response.send('Hello Avans!');
@@ -14,19 +15,29 @@ app.get('/about', function(request, response) {
     response.send('Written by Mark Meijer');
 })
 
-// app.post('/', function(request, response) {
-//     response.send('Hello Avans, POST request received!');
-// })
-//
-// app.put('/', function(request, response) {
-//     response.send('Hello Avans, PUT request received!');
-// })
+app.post('/', function(request, response) {
+    response.send('Hello Avans, POST request received!');
+})
+
+app.put('/', function(request, response) {
+    response.send('Hello Avans, PUT request received!');
+})
 
 app.all('*', function(request, response) {
     response.status(404);
     response.send('404 - Not found');
 })
 
-app.listen(3000, function() {
-    console.log('Server app is listening on port 3000');
-})
+// Start the server
+var port = process.env.PORT || app.get('PORT');
+
+app.listen(port, function() {
+    console.log('The magic happens at http://localhost:'+ port + ' !!!!!!!');
+});
+
+
+
+
+// app.listen(3000, function() {
+//     console.log('Server app is listening on port 3000');
+// })
